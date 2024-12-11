a979f6b80aaffb04b4b2a15ef689e27f7d1357bf
nguyentantai21042004 <tai21042004@gmail.com>
2024-12-11 21:12:00 +0700

    Final 01

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,6 +2,6 @@ export const environment = {
     prodution: true,
     // apiBaseUrl: 'https://co3103-programming-intergration-project-lmnw.onrender.com/api/v1',
     // apiBaseUrl: 'https://co3103-programming-intergration-project-8tdy.onrender.com/api/v1',
-    apiBaseUrl: 'http://localhost:8088/api/v1',
-    // apiBaseUrl: 'https://co3103-programming-intergration-project-8tdy.onrender.com/api/v1'
+    // apiBaseUrl: 'http://localhost:8088/api/v1',
+    apiBaseUrl: 'https://co3103-programming-intergration-project-8tdy.onrender.com/api/v1'
 }
