df26b839bdc715c68fb72fd6106c960117b073da
khanhngoolearn <tieukiayatoplanerso1vcs@gmail.com>
2024-12-14 21:01:36 +0700

    update search

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  // Mảng dữ liệu cho các mục
+  menuItems = [
+    { label: 'Everything', icon: '../../../assets/search-icon.png' },
+    { label: 'Tracks', icon: '../../../assets/track-icon.png' },
+    { label: 'People', icon: '../../../assets/people-icon.png' },
+    { label: 'Albums', icon: '../../../assets/album-icon.png' },
+    { label: 'Playlists', icon: '../../../assets/playlist.png' },
+  ];
 
+  // Chỉ số của mục đang được chọn
+  selectedIndex: number = 0;
+
+  // Hàm chọn mục
+  selectItem(index: number) {
+    this.selectedIndex = index; // Cập nhật chỉ số mục được chọn
+  }
 }
